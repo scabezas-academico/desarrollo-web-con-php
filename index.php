56a32de624e0294cf20d68c314ebc0a82637841e
Sebastián Cabezas Ríos <scabezas@zoidberg.local>
2026-04-23 13:49:31 -0400

    prueba de rama

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Programación Web</title>
+    <title>Programación Web en PHP con AdminLTE</title>
 
     <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
